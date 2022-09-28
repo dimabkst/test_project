39525f22810d16ba9cc2e05861442ee2c99ef0c2
Vision <dimabkst@gmail.com>
2022-09-28 21:39:52 +0300

    Changed / route.

--- express.js
+++ express.js
@@ -1,3 +1,5 @@
+const config = require('./config');
+
 const createError = require('http-errors');
 const session = require('express-session');
 const passport = require('./utils/passport');
@@ -7,9 +9,9 @@ const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
 const searchsRouter = require('./routes/searchs');
 const postsRouter = require('./routes/posts');
+const { restart } = require('nodemon');
 
 const app = express();
-
 
 app.use(express.json());
 app.use(session({
@@ -21,17 +23,23 @@ app.use(passport.authenticate('session'));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-app.get('/', async (req, res) => {
-    res.send({ message: "Everything works." });
+app.get('/', async (req, res, next) => {
+    try {
+        res.status(200).json({
+            status: 200,
+            data: {
+                message: (config.env == 'development') ? "I'm working" : "I'm simple social media backend API"
+            }
+        })
+    } catch (err) {
+        next(err);
+    }
 });
-
 
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 app.use('/searchs', searchsRouter);
 app.use('/posts', postsRouter);
-
 
 app.use((req, res, next) => {
     next(createError.NotFound());
@@ -44,6 +52,5 @@ app.use((err, req, res, next) => {
         message: err.message
     });
 });
-
 
 module.exports = app;
